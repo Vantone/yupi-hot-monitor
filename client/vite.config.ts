f17c5737e00a693fc7ff2e3364acfe95081117b4
Vantone <39580709+Vantone@users.noreply.github.com>
2026-05-06 01:19:07 +0800

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,6 +6,8 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
+    host: "0.0.0.0",
+    port: 8080
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
